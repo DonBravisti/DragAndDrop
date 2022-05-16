398e36a298a73db8d2ace91a69016493f21045f2
Бутов Станислав Юрьевич <46529@dc.nosu>
2022-05-16 10:30:53 +0300

    Добавьте файлы проекта.

---
+++ DragAndDrop/Cards.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragAndDrop
+{
+    public class Cards
+    {
+    }
+
+    public class Card: PictureBox
+    {
+        int id;
+
+        public Card(int id, Control control)
+        {
+            this.id = id;
+            this.Load("bin/Sources/Cards/p" + id.ToString() + ".png");
+            control.Controls.Add(this);
+            this.SizeMode = PictureBoxSizeMode.AutoSize;
+            this.MouseDown += Card_MouseDown;
+            this.MouseMove += Card_MouseMove;
+            this.MouseUp += Card_MouseUp;
+        }
+
+        private void Card_MouseUp(object sender, MouseEventArgs e)
+        {
+            FormMain.MovingCard = null;
+        }
+
+        int startX, startY;
+
+        private void Card_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - startX;
+                this.Top += e.Y - startY;
+            }
+        }
+
+        private void Card_MouseDown(object sender, MouseEventArgs e)
+        {
+            startX = e.X;
+            startY = e.Y;
+            FormMain.MovingCard = this;
+        }
+    }
+}
---
+++ DragAndDrop/FormMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragAndDrop
+{
+    public partial class FormMain : Form
+    {
+        public static Card MovingCard = null;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        Train t1;
+        Train t2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Card c1 = new Card(1, this);
+            t1 = new Train(2, this, 400, 20);
+            t2 = new Train(2, this, 400, 100);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (MovingCard != null && MovingCard.Bounds.IntersectsWith(t1.Bounds))
+            {
+                MovingCard.Location = new Point(1000, 1000);
+                MessageBox.Show("AAAAAAAAAAAAAAAAAAAAA");
+            }
+        }
+    }
+}
